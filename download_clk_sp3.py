899e91dcb54ec85dbe5051a155641033b6f69e04
lebedev_va <name_mercy@mail.ru>
2023-03-10 13:49:49 +0300

    delete

--- download_clk_sp3.py
+++ download_clk_sp3.py
@@ -1,30 +1,4 @@
 
-
-# import requests
-# from bs4 import BeautifulSoup as bs
-#
-# def download_clk_sp3(url_template):
-#
-#     req = requests.get(url_template)
-#
-#     if req.status_code == 200:
-#
-#         soup = bs(req.text, 'lxml')
-#
-#         for link in soup:
-#             print(link)
-#
-#
-#
-#
-# if __name__ == '__main__':
-#
-#
-#     url_template = "https://cddis.nasa.gov/archive/gnss/products/"
-#
-#     download_clk_sp3(url_template)
-
-
 import requests
 url = 'https://cddis.nasa.gov/archive/gnss/products/'
 values = {'username': 'vladimirPchelinc',
